41e6de444cd6dbae1c702ed7bcae1c61fe7d7001
Avi B <50595835+inspiringsource@users.noreply.github.com>
2026-02-04 13:46:02 +0100

    test: add multiple test cases

--- src/test/java/app/MainTest.java
+++ src/test/java/app/MainTest.java
@@ -7,10 +7,32 @@ import static org.junit.jupiter.api.Assertions.*;
 class MainTest {
 
     @Test
-    void getFeedback() {
+    void testOneCharCorrect() {
         String erratenesWort = "BRUGG";
         String zielwort = "BASEL";
         String erwartetesResultat = "GBBBB";
         assertEquals(erwartetesResultat, Main.getFeedback(erratenesWort, zielwort));
     }
+    @Test
+    void testTwoLastCharCorrect() {
+        String erratenesWort = "LOGIK";
+        String zielwort = "MUSIK";
+        String erwartetesResultat = "BBBGG";
+        assertEquals(erwartetesResultat, Main.getFeedback(erratenesWort, zielwort));
+    }
+    @Test
+    void testCharCorrect() {
+        String erratenesWort = "Stark";
+        String zielwort = "Stark";
+        String erwartetesResultat = "GGGGG";
+        assertEquals(erwartetesResultat, Main.getFeedback(erratenesWort, zielwort));
+    }
+    @Test
+    void testCharIncorrect() {
+        String erratenesWort = "STARK";
+        String zielwort = "BLUME";
+        String erwartetesResultat = "BBBBB";
+        assertEquals(erwartetesResultat, Main.getFeedback(erratenesWort, zielwort));
+    }
+
 }
